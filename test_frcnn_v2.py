864d8067c070956b171bb78a2fbfa76818a2ce67
KUN Ludovic <kunludovic@europe.essilor.group>
2018-05-29 17:07:55 +0200

    update functions.py

--- test_frcnn_v2.py
+++ test_frcnn_v2.py
@@ -22,7 +22,9 @@ parser.add_option("-n", "--num_rois", dest="num_rois",
 parser.add_option("--config_filename", dest="config_filename", help=
                 "Location to read the metadata related to the training (generated when training).",
                 default="config.pickle")
-
+parser.add_option("-b", "--bbox_threshold", dest="bbox_threshold", help="bbox_threshold", default=0.8)
+parser.add_option("-r", "--overlap_threshold_rpn", dest="overlap_threshold_rpn", help="overlap_threshold_rpn", default=0.7)
+parser.add_option("-c", "--overlap_thresh_classifier", dest="overlap_thresh_classifier", help="overlap_thresh_classifier", default=0.5)
 (options, args) = parser.parse_args()
 
 if not options.test_path and not options.test_image:   # if filename is not given or image is not given
@@ -56,9 +58,9 @@ C.num_rois = int(options.num_rois)
 # load models rpn and classifier
 model_rpn, model_classifier_only  = load_models_for_test(C, class_mapping)
 
-bbox_threshold = 0.8
-overlap_threshold_rpn = 0.7
-overlap_threshold_class = 0.7
+bbox_threshold = float(options.bbox_threshold)
+overlap_threshold_rpn = float(options.overlap_thresh_rpn)
+overlap_threshold_class = float(options.overlap_thresh_classifier)
 
 if options.test_path is not None:
     img_path = options.test_path
@@ -89,8 +91,9 @@ if options.test_image is not None:
     img, all_dets = predict_on_image(img, model_rpn, model_classifier_only, C, class_mapping_inv, class_to_color, bbox_threshold=bbox_threshold, overlap_thresh_rpn=overlap_threshold_rpn, overlap_thresh_classifier=overlap_threshold_class)
     print('Elapsed time = {}'.format(time.time() - st))
     print(all_dets)
-    cv2.imshow('img', img)
+    
     if saved == False:
+        cv2.imshow('img', img)
         cv2.waitKey(0)
     else:
         cv2.imwrite('test.jpg',img)
